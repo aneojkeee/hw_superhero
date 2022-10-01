daa8bcd9ce05912c546b44b5e72ade4d0c454ee7
Vladislav Mashin <mashinvladislav@gmail.com>
2022-10-01 12:44:48 +0300

    second commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 heroes_list = ['Hulk', 'Captain america', 'Thanos']
 intelligence_dict = {'Hulk': 0, 'Captain america': 0, 'Thanos': 0}
 url = 'https://www.superheroapi.com/api.php/2619421814940190/search/'
+
 for hero in heroes_list:
     hero_dict = json.loads(requests.get(url + hero).content)
     intelligence_dict[hero] = int(hero_dict['results'][0]['powerstats']['intelligence'])
-print(intelligence_dict[hero])
+print(f'Самый интелектуальный: {hero}, интеллект- {intelligence_dict[hero]}')
